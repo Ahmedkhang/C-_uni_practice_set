77c2b1bad25fb317b3f18518b146efeac9a1e5ac
Ahmed Ur Rehman <160028006+Ahmedkhang@users.noreply.github.com>
2026-06-11 22:47:00 -0700

    Add files via upload

---
+++ Final_practice_set/exam.cpp
@@ -0,0 +1,133 @@
+#include <stdio.h>
+int main(){
+
+
+//	int table,result;
+//	printf("enter table no: ");
+//	scanf("%d",&table);
+//	for(int i =1;i<=10;i++){
+//		result = table * i;
+//		printf("%d X %d = %d\n",table,i,result);
+//	}
+
+
+	
+//	int n,result =0;
+//	printf("ENter  anumber: ");
+//	scanf("%d",&n);
+//	for(int i=1; i<=n;i++){
+//		result = result + i;
+//		printf("%d\n",result);
+//	}
+
+
+
+//int arr[5];
+//printf("Enter 5 nnumbers: ");
+//for(int i=0;i<5;i++){
+//	scanf("%d",&arr[i]);
+//}
+//
+//int largest = arr[0];
+//int smallest = arr[0];
+//
+//for(int i = 0; i< 5; i++){
+//	if(arr[i] > largest){
+//		 largest = arr[i];
+//	}
+//	if(arr[i] < smallest){
+//		smallest = arr[i] ;
+//	}
+//	
+//}
+
+//printf("Smallest = %d\nLargest = %d",smallest,largest);
+
+
+	
+//	
+//	int arr[5];
+//	printf("Enter 5 numbers: ");
+//	for(int i = 0; i<5;i++){
+//		scanf("%d",&arr[i]);
+//	}
+//	int start = 0;
+//	int end = 4;
+//	
+//	while(start < end){
+//		int temp = arr[start];
+//		arr[start] = arr[end];
+//		arr[end] = temp;
+//		start++;
+//		end--;
+//	}
+//	for(int i=0; i<5; i++){
+//    printf("%d ", arr[i]);
+//}
+
+
+
+
+//int arr[5] = {1,2,3,4,5};
+//int target;
+//bool found = false;
+//
+//printf("ENter a number to search: ");
+//scanf("%d",&target);
+//
+//for(int i =0; i<5; i++){
+//	if(arr[i] == target){
+//		printf("Found at index %d\n",i);
+//		found = true;
+//		break;
+//	}
+//}
+//if(!found){
+//	printf("Did not found %d",target);
+//}
+
+
+
+//	int sum =0;
+//	for(int i = 1;i<=10;i++){
+//		sum += i;
+//	}
+//	
+//	printf("%d",sum);
+
+//int arr[5];
+//int even =0,odd =0;
+//
+//printf("Enter 5 numbers: ");
+//for(int i =0; i<5;i++){
+//	scanf("%d",&arr[i]);
+//}
+//
+//for(int i =0; i<5;i++){
+//	if(arr[i] %2 ==0){
+//		even++;
+//	}else{
+//		odd++;
+//	}
+//}
+//
+//printf("Odd numbers = %d\nEven number = %d",odd,even);
+//
+//
+
+//int arr[5];
+//int sum,average;
+//printf("Enter 5 numbers: ");
+//for(int i = 0; i<5; i++){
+//	scanf("%d",&arr[i]);
+//}
+//for(int i =0; i<5; i++){
+//	sum += arr[i];
+//	
+//}
+//average = sum / 5;
+//
+//printf("SUm Of array is %d\nAverage is %d",sum,average);
+
+
+}
